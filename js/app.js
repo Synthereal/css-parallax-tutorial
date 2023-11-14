47e2f13fee31b10e14e254fb1fe563d7dca1d636
Synthereal <infernofire027@gmail.com>
2023-11-13 22:25:39 -0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,98 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const parallax_el = document.querySelectorAll(".parallax");
+  const main = document.querySelector("main");
+
+  let xValue = 0,
+    yValue = 0,
+    rotateDegree = 0;
+
+  function update(cursorPosition) {
+    parallax_el.forEach((el) => {
+      let speedx = el.dataset.speedx;
+      let speedy = el.dataset.speedy;
+      let speedz = el.dataset.speedz;
+      let rotateSpeed = el.dataset.rotation;
+
+      let isInLeft =
+        // determines if element is on left half of screen or not
+        parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1; // 1 if left, -1 if right
+      let zValue =
+        (cursorPosition - parseFloat(getComputedStyle(el).left)) *
+        isInLeft *
+        0.1;
+
+      el.style.transform = `perspective(2300px) translateZ(${
+        zValue * speedz
+      }px) rotateY(${rotateDegree * rotateSpeed}deg) translateX(calc(-50% + ${
+        -xValue * speedx
+      }px)) translateY(calc(-50% + ${-yValue * speedy}px))`;
+    });
+  }
+
+  update(0);
+
+  window.addEventListener("mousemove", (e) => {
+    if (timeline.isActive()) return; // pause parallax effect until load animation is completed
+
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
+    update(e.clientX);
+  });
+
+  if (window.innerWidth >= 725) {
+    main.style.maxHeight = `${window.innerWidth * 0.6}px`;
+  } else {
+    main.style.maxHeight = `${window.innerWidth * 1.6}px`;
+  }
+
+  // GSAP Load Animation
+
+  let timeline = gsap.timeline();
+
+  Array.from(parallax_el)
+    .filter((el) => !el.classList.contains("text"))
+    .forEach((el) => {
+      timeline.from(
+        el,
+        {
+          top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+          duration: 3,
+          ease: "power3.out",
+        },
+        "1"
+      );
+    });
+
+  timeline
+    .from(
+      ".text h1",
+      {
+        y:
+          window.innerHeight -
+          document.querySelector(".text h1").getBoundingClientRect().top +
+          200,
+        duration: 2,
+      },
+      "2.5"
+    )
+    .from(
+      ".text h2",
+      {
+        y: -150,
+        opacity: 0,
+        duration: 1.5,
+      },
+      "3"
+    )
+    .from(
+      ".hide",
+      {
+        opacity: 0,
+        duration: 1.5,
+      },
+      "3"
+    );
+});
